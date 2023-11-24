81a428a02062e4ea52cf2a4cd7555705b24ca81f
Ayan Maji <88241719+theayan44@users.noreply.github.com>
2023-11-24 18:45:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -12,4 +12,25 @@ const apiUrls = [
   "https://jsonplaceholder.typicode.com/todos/10",
 ];
 
+const outputAll = document.getElementById("output-all");
+const outputAny = document.getElementById("output-any");
+
 // You can write your code here
+function compare(){
+	const fetches = [];
+	let startTime = performance.now();
+	for(let i=0; i<apiUrls.length; i++){
+		fetches[i] = fetch(apiUrls[i]);
+	}
+	Promise.all(fetches).then((values)=>{
+		const endTime = performance.now();
+		outputAll.innerText = endTime - startTime;
+	});
+	startTime = performance.now();
+	Promise.any(fetches).then((values)=>{
+		const endTime = performance.now();
+		outputAny.innerText = endTime - startTime;
+	});
+}
+
+compare();
